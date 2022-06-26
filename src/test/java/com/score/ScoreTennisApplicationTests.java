8ea64bfbd39656dd5a93832b953931ca511f5b37
tallapenda <talladiopp@gmail.com>
2022-06-27 00:32:00 +0100

    ajout fichier teste

--- src/test/java/com/score/ScoreTennisApplicationTests.java
+++ src/test/java/com/score/ScoreTennisApplicationTests.java
@@ -1,13 +1,25 @@
 package com.score;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.mockito.Mockito.reset;
 
+import org.junit.jupiter.api.Test;
+import com.score.Jeux;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import com.score.InterfaceScore;
 @SpringBootTest
 class ScoreTennisApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Jeux gt = new Jeux("Talla", "Piere");
+		gt.reset();
+		gt.play(1);
+		gt.showScores();
+		
 	}
+	
+	
+	
 
 }
